66f5393d191e9772b55e14212eb4c44260c521c3
Jason Raimondi <jason@raimondi.us>
2018-07-26 22:42:31 -0700

    Wip trying to get nunjucks.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
+import * as express from 'express';
+import * as nunjucks from 'nunjucks';
+import { join } from 'path';
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApplicationModule);
+  const server: any = express();
+  const templatePath = join(__dirname, 'templates');
+  
+  nunjucks.configure(templatePath, {
+    autoescape: true,
+    express: server
+  });
+  
+  const app = await NestFactory.create(ApplicationModule, server);
+  app.use(express.static(join(__dirname, '../assets')));
   await app.listen(3000);
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
 }
 bootstrap();
