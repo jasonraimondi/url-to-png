f67c8941a19c11cf1b32b62045acee4e19dc74ad
Jason Raimondi <jason@raimondi.us>
2018-09-26 17:21:58 -0700

    Adding helmet and express-rate-limiting.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as rateLimit from 'express-rate-limit';
+import * as helmet from 'helmet';
+
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 import { winstonLogger } from './winston-logger';
@@ -6,6 +9,11 @@ async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule, {
     logger: winstonLogger,
   });
+  app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }));
+  app.use(helmet());
   await app.listen(3000);
 }
 
