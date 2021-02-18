5a6d75548859daadd4e4cc641a0f22d16959d21c
Jason Raimondi <jason@raimondi.us>
2021-02-18 14:18:28 -0800

    chore: bump deps and fix updated apis

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
 import { Options } from 'generic-pool';
 import * as nano from 'nano';
-import { NavigationOptions } from 'puppeteer';
+import { WaitForOptions } from 'puppeteer';
 
 import { AppController } from './controllers/app.controller';
 import { createPuppeteerPool } from './puppeteer-pool';
@@ -65,7 +65,7 @@ const imageRenderService = {
       opts.max = Number(process.env.POOLS_MAX);
     }
 
-    const navigationOptions: NavigationOptions = {};
+    const navigationOptions: WaitForOptions = {};
     switch (process.env.PUPPETEER_WAIT_UNTIL) {
       case 'load':
       case 'domcontentloaded':
