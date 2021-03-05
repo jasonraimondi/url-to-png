fdb1c71b511fbc2540eb7aa60ae79e62d1229448
Jason Raimondi <jason@raimondi.us>
2021-03-05 14:33:19 -0800

    refactor: move allow list to guard
    middlewares are more for authentication
    guards are more for authorization
    
    since the allow list is more of an authorization grant, it would be better as a guard.

--- src/config.api.ts
+++ src/config.api.ts
@@ -1,3 +1,5 @@
+import { IsBooleanString, IsNumberString, IsOptional, IsUrl } from 'class-validator';
+
 export interface IConfigAPI {
   width?: number;
   height?: number;
@@ -10,14 +12,42 @@ export interface IConfigAPI {
 }
 
 export class ConfigApi implements IConfigAPI {
+  @IsUrl()
   public readonly url: string;
+
+  @IsOptional()
+  @IsBooleanString()
   public readonly forceReload?: boolean;
+
+  @IsOptional()
+  @IsNumberString()
   public readonly width?: number;
+
+  @IsOptional()
+  @IsNumberString()
   public readonly height?: number;
+
+  @IsOptional()
+  @IsNumberString()
   public readonly viewPortHeight?: number;
+
+  @IsOptional()
+  @IsNumberString()
   public readonly viewPortWidth?: number;
+
+  @IsOptional()
+  @IsBooleanString()
   public readonly isMobile?: boolean;
+
+  @IsOptional()
+  @IsBooleanString()
   public readonly isFullPage?: boolean;
+
+  @IsOptional()
+  @IsBooleanString()
   public readonly isDarkMode?: boolean;
+
+  @IsOptional()
+  @IsNumberString()
   public readonly deviceScaleFactor?: number;
 }
